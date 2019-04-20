538c5de059e2a590b18593ad0377416c3f1944e3
N3ov <46546577+N3ov@users.noreply.github.com>
2019-04-20 15:22:14 +0800

    Add files via upload

--- get_unemployment_rate.py
+++ get_unemployment_rate.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
+
 def main():
     start = sys.argv[1]
     end = sys.argv[2]
@@ -9,7 +10,7 @@ def main():
     print('結束民國與月份: %s' % (end))
 
     # url = 'https://statdb.mol.gov.tw/statis/jspProxy.aspx?sys=220&ym=6701&ymt=10802&funid=q02072'
-    url = 'https://statdb.mol.gov.tw/statis/jspProxy.aspx?sys=220&ym={0}&ymt={1}&funid=q02072'.format(start,end)
+    url = 'https://statdb.mol.gov.tw/statis/jspProxy.aspx?sys=220&ym={0}&ymt={1}&funid=q02072'.format(start, end)
     # urls = ['https://www.stat.gov.tw/lp.asp?CtNode=519&CtUnit=1818&BaseDSD=29&nowPage={}' .format(str(i)) for i in range(1,9)]
     response = requests.get(url)
     doc = BeautifulSoup(response.text, 'html.parser')
@@ -24,20 +25,23 @@ def main():
         # print(result.get_text())
         rate.append(result.get_text())
         count += 1
-    print(rate)
-    print('總計月份數: ',count)
+    # print(rate)
+    print('總計月份數: ', count)
+
+    turn = list(map(float, rate))
+    print(turn)
+
+
+    # data = [go.Scatter(y=turn[''])]
+    # py.iplot(data, filename='time-series-simple')
+
+
         # unemp = result.select_one('td[class="stymon stydata"]')
         # print(unemp)
         # match = re.compile(r'(\d+(\.\d+)?)', unemp)
-
-
         # print(match)
 
-
-
-
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
 
 
